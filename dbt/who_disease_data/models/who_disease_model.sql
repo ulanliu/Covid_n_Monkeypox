a39ffb2a96cfd3b76ec51faffba869e7cbe3cb96
ulanliu <f97290650@gmail.com>
2023-03-28 06:26:30 +0000

    Update dbt model

--- dbt/who_disease_data/models/who_disease_model.sql
+++ dbt/who_disease_data/models/who_disease_model.sql
@@ -15,26 +15,38 @@ covid19 AS (
         {% for col in cols %}
             covid19.{{col.name}} AS covid19_{{col.name}},
         {% endfor %}
-        -- location,
-        -- iso_code,
-        -- date,
-        -- total_cases,
-        -- total_deaths,
-        -- new_cases,
-        -- new_deaths,
-        -- new_cases_smoothed,
-        -- new_deaths_smoothed,
-        -- new_cases_per_million,
-        -- total_cases_per_million,
-        -- new_cases_smoothed_per_million,
-        -- new_deaths_per_million,
-        -- total_deaths_per_million,
-        -- new_deaths_smoothed_per_million
     FROM {{ source('staging', 'covid19') }}
 )
 
-SELECT * 
+SELECT 
+    covid19_location AS location,
+    covid19_iso_code AS iso_code,
+    covid19_date AS date,
+    monkeypox_total_cases,
+    monkeypox_total_deaths,
+    monkeypox_new_cases,
+    monkeypox_new_deaths,
+    monkeypox_new_cases_smoothed,
+    monkeypox_new_deaths_smoothed,
+    monkeypox_new_cases_per_million,
+    monkeypox_total_cases_per_million,
+    monkeypox_new_cases_smoothed_per_million,
+    monkeypox_new_deaths_per_million,
+    monkeypox_total_deaths_per_million,
+    monkeypox_new_deaths_smoothed_per_million,
+    covid19_total_cases,
+    covid19_total_deaths,
+    covid19_new_cases,
+    covid19_new_deaths,
+    covid19_new_cases_smoothed,
+    covid19_new_deaths_smoothed,
+    covid19_new_cases_per_million,
+    covid19_total_cases_per_million,
+    covid19_new_cases_smoothed_per_million,
+    covid19_new_deaths_per_million,
+    covid19_total_deaths_per_million,
+    covid19_new_deaths_smoothed_per_million
 FROM covid19 AS c
 LEFT JOIN monkeypox AS m
     ON c.covid19_iso_code = m.monkeypox_iso_code AND c.covid19_date = m.monkeypox_date
-
+WHERE covid19_location != "World"
