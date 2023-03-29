92efae4d68896a448358cce5b723a885641a3524
ulanliu <f97290650@gmail.com>
2023-03-29 08:09:43 +0000

    Update prefect_dbt_flow

--- prefect/flows/prefect_dbt_flow.py
+++ prefect/flows/prefect_dbt_flow.py
@@ -1,5 +1,9 @@
 from prefect import flow
 from prefect_dbt.cli.commands import trigger_dbt_cli_command
+import os 
+
+patched_path_env = "/home/ulan/anaconda3/envs/zoomcamp/bin:{}".format(os.environ["PATH"])
+os.environ["PATH"] = patched_path_env
 
 @flow(retries=3)
 def trigger_dbt_cli_command_flow(command:str):
