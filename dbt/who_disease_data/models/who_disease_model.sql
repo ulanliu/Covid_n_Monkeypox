5746dca92c4c3cbcd5e94a3485b7201a5e674d55
ulanliu <f97290650@gmail.com>
2023-03-30 09:38:56 +0000

    Add comments and adjust variable name

--- dbt/who_disease_data/models/who_disease_model.sql
+++ dbt/who_disease_data/models/who_disease_model.sql
@@ -4,49 +4,27 @@
 
 WITH monkeypox AS (
     SELECT
-        {% for col in cols %}
-            monkeypox.{{col.name}} AS monkeypox_{{col.name}},
-        {% endfor %}
+        *,
+        'monkeypox' AS disease_type
     FROM {{ source('staging', 'monkeypox') }}
 ),
 
 covid19 AS (
     SELECT 
         {% for col in cols %}
-            covid19.{{col.name}} AS covid19_{{col.name}},
+            covid19.{{col.name}} AS {{col.name}},
         {% endfor %}
+        'covid19' AS disease_type
     FROM {{ source('staging', 'covid19') }}
+),
+
+diseases_unioned AS (
+    SELECT * FROM monkeypox
+    UNION ALL
+    SELECT * FROM covid19
 )
 
 SELECT 
-    covid19_location AS location,
-    covid19_iso_code AS iso_code,
-    covid19_date AS date,
-    monkeypox_total_cases,
-    monkeypox_total_deaths,
-    monkeypox_new_cases,
-    monkeypox_new_deaths,
-    monkeypox_new_cases_smoothed,
-    monkeypox_new_deaths_smoothed,
-    monkeypox_new_cases_per_million,
-    monkeypox_total_cases_per_million,
-    monkeypox_new_cases_smoothed_per_million,
-    monkeypox_new_deaths_per_million,
-    monkeypox_total_deaths_per_million,
-    monkeypox_new_deaths_smoothed_per_million,
-    covid19_total_cases,
-    covid19_total_deaths,
-    covid19_new_cases,
-    covid19_new_deaths,
-    covid19_new_cases_smoothed,
-    covid19_new_deaths_smoothed,
-    covid19_new_cases_per_million,
-    covid19_total_cases_per_million,
-    covid19_new_cases_smoothed_per_million,
-    covid19_new_deaths_per_million,
-    covid19_total_deaths_per_million,
-    covid19_new_deaths_smoothed_per_million
-FROM covid19 AS c
-LEFT JOIN monkeypox AS m
-    ON c.covid19_iso_code = m.monkeypox_iso_code AND c.covid19_date = m.monkeypox_date
-WHERE covid19_location != "World"
+    *
+FROM diseases_unioned
+WHERE location != "World"
